5ba7b491be08ce9e4686d4f1e1c3ce04df00ccb1
Tugba Musaak <115474345+musaak-tugba@users.noreply.github.com>
2022-10-10 19:22:01 +0300

    Java Kampı 2022
    Lesson 1 - Homework 2

---
+++ Lesson1-Homework2/sayiBulma/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int[] sayilar = new int [] {1,2,5,7,9,0};
+        int aranacak = 5;
+        boolean varMi = false;
+
+        for ( int sayi : sayilar){
+            if(sayi==aranacak){
+                varMi = true;
+                break;
+            }
+        }
+
+        if(varMi){
+            System.out.println("Sayı var");
+        }else{
+            System.out.println("Sayı yoktur");
+        }
+    }
+}
